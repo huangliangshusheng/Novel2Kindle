8fa4bc0194a92bc327c683d74e36f43de4e91a15
huangliangshusheng <huangliangshusheng@gmail.com>
2021-09-30 19:51:21 +0800

    update

--- main.py
+++ main.py
@@ -20,96 +20,149 @@ session.headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
 }
 
-pattern = r"\n {2}(\S.*)\r\n([\S\s]+?)\r\n\r"
-
-
-def wenku_spider(novel):
-    def create_section(volume):
-        vid = "".join(
-            volume.xpath("../following-sibling::tr[1]/td[1]/a/@href")
-        ).replace(".htm", "")
-        if not vid:
-            return None
-
-        section_title = f"{title} {volume.text}"
-        section_url = f"https://dl.wenku8.com/packtxt.php?aid={aid}&vid={vid}&charset=utf-8"
-
-        response = fetch(section_url)
-        text_list = re.findall(pattern, response.text)
-        article_list = list(filter(None, (
-            create_article(f"{section_title}-{index}", text)
-            for index, text in enumerate(text_list)
-        )))
-
-        return Section(section_title, article_list)
-
-    def create_article(id, text):
-        line_list = text[1].split("\n")
-        description, content = format_content(line_list)
-        if not description:
-            return None
-
-        article = Article(id, text[0], description)
-        write_Article(article, content)
-        return article
-
-    aid = novel["aid"]
-    title = novel["title"]
-    last_index = novel.get("last_index", -1)
-    url = f"https://www.wenku8.net/novel/{aid // 1000}/{aid}/index.htm"
-
-    volume_list = get_list(url, "//td[@class='vcss']")
-    novel["last_index"] = len(volume_list)
-    section_list = list(
-        filter(None, map(create_section, volume_list[last_index:]))
-    )
-
-    return section_list
-
-
-def default_spider(novel, chapter_xpath, content_xpath):
-    def create_article(id, chapter):
-        chapter_title = chapter.xpath("./child::node()")[0]
-        chapter_url = chapter.xpath("./ancestor-or-self::node()/@href")[0]
-
-        line_list = get_list(chapter_url, content_xpath)
-        description, content = format_content(line_list)
-        article = Article(id, chapter_title, description)
-        write_Article(article, content)
-
-        return article
-
-    url = novel["url"]
-    title = novel["title"]
-    last_index = novel.get("last_index", -1)
-
-    chapter_list = get_list(url, chapter_xpath, True)
-    novel["last_index"] = len(chapter_list)
-    article_list = list(
-        create_article(f"{title}-{index}", chapter)
-        for index, chapter in enumerate(chapter_list[last_index:])
-    )
-
-    if article_list:
-        return [Section(title, article_list)]
-    else:
-        return []
-
 
 def create_section_list(domain):
     novel_list = domain["novel_list"]
+    chapter_pattern = domain["chapter_pattern"]
+    content_pattern = domain["content_pattern"]
 
     if "wenku" == domain["domain"]:
-        section_list = map(wenku_spider, novel_list)
+        create_section = create_wenku_parser(
+            chapter_pattern, content_pattern
+        )
     else:
-        chapter_xpath = domain["chapter_xpath"]
-        content_xpath = domain["content_xpath"]
-        section_list = map(
-            lambda novel: default_spider(novel, chapter_xpath, content_xpath),
-            novel_list
+        create_section = create_default_parser(
+            chapter_pattern, content_pattern
         )
 
-    return merge_list(section_list)
+    return filter_map(create_section, novel_list)
+
+
+def create_wenku_parser(chapter_pattern, content_pattern):
+    content_pattern = re.compile(content_pattern)
+
+    def create_article(id, chapter):
+        title, aid, vid = chapter
+        url = f"https://dl.wenku8.com/packtxt.php?aid={aid}&vid={vid}&charset=utf-8"
+
+        response = fetch(url)
+        text = re.search(content_pattern, response.text).group(1)
+        line_list = text.split("\r\n")
+        content_list = filter_map(lambda line: line.strip(), line_list)
+        if not content_list:
+            return None
+
+        description = content_list[0]
+        content = "<br>".join(
+            map(lambda line: f"<p>&nbsp;&nbsp;{line}</p>", content_list)
+        )
+        article = Article(id, title, description)
+        write_Article(article, content)
+
+        return article
+
+    def create_section(novel):
+        aid = novel["aid"]
+        title = novel["title"]
+        last_index = novel.get("last_index", -1)
+        url = f"https://www.wenku8.net/novel/{aid // 1000}/{aid}/index.htm"
+
+        response = fetch(url)
+        parser = html.fromstring(response.content)
+        link_list = parser.xpath(chapter_pattern)
+        novel["last_index"] = len(link_list)
+        chapter_list = filter_map(
+            lambda link: (
+                link.xpath("./child::node()")[0],
+                aid,
+                link.xpath(
+                    "./ancestor-or-self::node()/@href"
+                )[0].replace(".htm", "")
+            ),
+            link_list[last_index:]
+        )
+        article_list = filter_map(
+            lambda index, chapter: create_article(f"{title}-{index}", chapter),
+            range(len(chapter_list)), chapter_list
+        )
+        if not article_list:
+            return None
+
+        return Section(title, article_list)
+
+    return create_section
+
+
+def create_default_parser(chapter_pattern, content_pattern):
+    def create_article(id, chapter):
+        title, url = chapter
+
+        response = fetch(url)
+        parser = html.fromstring(response.content)
+        line_list = parser.xpath(content_pattern)
+        content_list = filter_map(lambda line: line.strip(), line_list)
+        if not content_list:
+            return None
+
+        description = content_list[0]
+        content = "<br>".join(
+            map(lambda line: f"<p>&nbsp;&nbsp;{line}</p>", content_list)
+        )
+        article = Article(id, title, description)
+        write_Article(article, content)
+
+        return article
+
+    def create_section(novel):
+        url = novel["url"]
+        title = novel["title"]
+        last_index = novel.get("last_index", -1)
+
+        response = fetch(url)
+        parser = html.fromstring(response.content)
+        parser.make_links_absolute(base_url=url)
+        link_list = parser.xpath(chapter_pattern)
+        novel["last_index"] = len(link_list)
+        chapter_list = filter_map(
+            lambda link: (
+                link.xpath("./child::node()")[0],
+                link.xpath("./ancestor-or-self::node()/@href")[0]
+            ),
+            link_list[last_index:]
+        )
+        article_list = filter_map(
+            lambda index, chapter: create_article(f"{title}-{index}", chapter),
+            range(len(chapter_list)), chapter_list
+        )
+        if not article_list:
+            return None
+
+        return Section(title, article_list)
+
+    return create_section
+
+
+def write_Article(article, content):
+    html = f'''
+            <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+                <head>
+                    <meta content="http://www.w3.org/1999/xhtml; charset=utf-8" http-equiv="Content-Type" />
+                    <title>{article.title}</title>
+                </head>
+                <body>
+                    <div id="section1"></div>
+                    <h1 id="title1" height="1em">
+                        <font size="7">
+                            <b>{article.title}</b>
+                        </font>
+                    </h1>
+                    <br>
+                    {content}
+                </body>
+            </html>'''
+
+    filename = f"html/{article.id}.html"
+    write_file(filename, html)
 
 
 def write_magazine(manazine):
@@ -204,76 +257,6 @@ def write_magazine(manazine):
     write_toc_html()
 
 
-@retry(stop=stop_after_attempt(3), wait=wait_random(min=1, max=3))
-def fetch(url):
-    response = session.get(url, timeout=5)
-    if(response.status_code > 399):
-        raise IOError("connect error!")
-
-    return response
-
-
-def get_list(url, xpath, absolute_link=False):
-    response = fetch(url)
-    parser = html.fromstring(response.content)
-    if absolute_link:
-        parser.make_links_absolute(base_url=url)
-    return parser.xpath(xpath)
-
-
-def format_content(line_list):
-    content_list = tuple(
-        filter(None, map(lambda line: line.strip(), line_list))
-    )
-    if not content_list:
-        return None, None
-
-    description = content_list[0]
-    content = "<br>".join(
-        map(lambda line: f"<p>&nbsp;&nbsp;{line}</p>", content_list)
-    )
-    return description, content
-
-
-def load_json(filename):
-    with open(filename, mode="r", encoding="utf-8") as f:
-        novel_list = json.load(f)
-    return novel_list
-
-
-def dump_json(novel_list, filename):
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(novel_list, f, ensure_ascii=False)
-
-
-def write_file(filename, content):
-    with open(filename, mode='w', encoding="utf-8") as f:
-        f.write(content)
-
-
-def write_Article(article, content):
-    html = f'''
-            <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-                <head>
-                    <meta content="http://www.w3.org/1999/xhtml; charset=utf-8" http-equiv="Content-Type" />
-                    <title>{article.title}</title>
-                </head>
-                <body>
-                    <div id="section1"></div>
-                    <h1 id="title1" height="1em">
-                        <font size="7">
-                            <b>{article.title}</b>
-                        </font>
-                    </h1>
-                    <br>
-                    {content}
-                </body>
-            </html>'''
-
-    filename = f"html/{article.id}.html"
-    write_file(filename, html)
-
-
 def article_to_opf(article):
     item = f"<item href='html/{article.id}.html' media-type='application/xhtml+xml' id='{article.id}'/>"
     itemref = f"<itemref idref='{article.id}'/>"
@@ -322,8 +305,33 @@ def section_to_toc_html(section):
         </ul>'''
 
 
-def merge_list(list):
-    return reduce(lambda x, y: x+y, list)
+def filter_map(func, *iterables):
+    return list(filter(None, map(func, *iterables)))
+
+
+@retry(stop=stop_after_attempt(3), wait=wait_random(min=1, max=3))
+def fetch(url):
+    response = session.get(url, timeout=5)
+    if(response.status_code > 399):
+        raise IOError("connect error!")
+
+    return response
+
+
+def load_json(filename):
+    with open(filename, mode="r", encoding="utf-8") as f:
+        novel_list = json.load(f)
+    return novel_list
+
+
+def dump_json(novel_list, filename):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(novel_list, f, ensure_ascii=False)
+
+
+def write_file(filename, content):
+    with open(filename, mode='w', encoding="utf-8") as f:
+        f.write(content)
 
 
 if __name__ == "__main__":
@@ -331,11 +339,14 @@ if __name__ == "__main__":
 
     setting = load_json("setting.json")
 
-    section_list = merge_list(map(create_section_list, setting["domain_list"]))
+    section_list = sum(
+        map(create_section_list, setting["domain_list"]), []
+    )
 
-    if len(section_list) > 0:
-        magazine = Magazine(title=setting.get(
-            "title", "Novel"), section_list=section_list)
+    if section_list:
+        magazine = Magazine(
+            title=setting.get("title", "Novel"), section_list=section_list
+        )
 
         write_magazine(magazine)
 
